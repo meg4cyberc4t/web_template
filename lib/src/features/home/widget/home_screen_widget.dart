47235f20ca64014d67e180fae2fd0b03419a6a39
Igor Molchanov <66907532+meg4cyberc4t@users.noreply.github.com>
2024-03-16 01:33:08 +0300

    Update home_screen_widget.dart

--- lib/src/features/home/widget/home_screen_widget.dart
+++ lib/src/features/home/widget/home_screen_widget.dart
@@ -1,9 +1,6 @@
 // ignore_for_file: public_member_api_docs
 
-import 'dart:js_interop' show NullableObjectUtilExtension;
-
 import 'package:flutter/material.dart';
-import 'package:web/web.dart' hide Text;
 import 'package:web_template/src/common/localizations/localizations_state_mixin.dart';
 
 class HomeScreenWidget extends StatelessWidget {
@@ -16,22 +13,6 @@ class HomeScreenWidget extends StatelessWidget {
         ),
         body: Center(
           child: Text(context.localizations.title),
-        ),
-        floatingActionButton: FloatingActionButton.extended(
-          label: const Text('Source Code Repository'),
-          onPressed: () {
-            console.log(DateTime.now().jsify());
-            for (int i = 0; i < 9999999; i++) {
-              for (int l = 0; l < 99999999; l++) {
-                for (int k = 0; k < 9999999; k++) {
-                  final ans = k * l * i;
-                  console.log(ans.jsify());
-                }
-              }
-            }
-            console.log(DateTime.now().jsify());
-            //TODO:!
-          },
         ),
       );
 }
