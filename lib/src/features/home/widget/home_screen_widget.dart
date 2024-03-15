5d0d790e2f09834fb6fe0f0a1dfbc01a2ac6c408
Igor Molchanov <66907532+meg4cyberc4t@users.noreply.github.com>
2024-03-16 01:22:30 +0300

    _

--- lib/src/features/home/widget/home_screen_widget.dart
+++ lib/src/features/home/widget/home_screen_widget.dart
@@ -1,6 +1,10 @@
 // ignore_for_file: public_member_api_docs
 
+import 'dart:js_interop' show NullableObjectUtilExtension;
+
 import 'package:flutter/material.dart';
+import 'package:web/web.dart' hide Text;
+import 'package:web_template/src/common/localizations/localizations_state_mixin.dart';
 
 class HomeScreenWidget extends StatelessWidget {
   const HomeScreenWidget({super.key});
@@ -8,7 +12,26 @@ class HomeScreenWidget extends StatelessWidget {
   @override
   Widget build(final BuildContext context) => Scaffold(
         appBar: AppBar(
-          title: const Text('HomeScreenWidget'),
+          title: Text(context.localizations.title),
+        ),
+        body: Center(
+          child: Text(context.localizations.title),
+        ),
+        floatingActionButton: FloatingActionButton.extended(
+          label: const Text('Source Code Repository'),
+          onPressed: () {
+            console.log(DateTime.now().jsify());
+            for (int i = 0; i < 9999999; i++) {
+              for (int l = 0; l < 99999999; l++) {
+                for (int k = 0; k < 9999999; k++) {
+                  final ans = k * l * i;
+                  console.log(ans.jsify());
+                }
+              }
+            }
+            console.log(DateTime.now().jsify());
+            //TODO:!
+          },
         ),
       );
 }
---
+++ test/main_test.dart
@@ -0,0 +1,3 @@
+void main() {
+  // pass test here
+}
